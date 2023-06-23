8f5a1eb077daaaece0c6e8037dda61b55b29f6dc
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-06-23 22:11:01 +0530

    Add files via upload

---
+++ Day 0/SumOfThreeNumbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class sum {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int a=obj.nextInt();
+        int b=obj.nextInt();
+        int c=obj.nextInt();
+        System.out.println((a+b)+"\n"+(a+c)+"\n"+(b+c));
+    }
+}
