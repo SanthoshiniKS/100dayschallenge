f8dbc9ac2dee5c6a230985652c212949c20cd12f
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-06-23 22:10:21 +0530

    Delete areaofcircle.java

--- Day 0/areaofcircle.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class area {
-    public static void main(String[] args) {
-        Scanner obj=new Scanner(System.in);
-        int r=obj.nextInt();
-        System.out.format("%.2f",Math.PI*r*r);
-    }
-}
