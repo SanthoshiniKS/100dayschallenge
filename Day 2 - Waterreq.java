be714ee52d90dde03b13c3557e566ac88da51356
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-06-24 11:03:39 +0530

    Add files via upload

---
+++ Day 2 - Waterreq.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Waterreq {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        for(int i=0;i<n;i++)
+        {
+             int h=obj.nextInt();
+             System.out.println(h*2);
+        }
+    }
+}
