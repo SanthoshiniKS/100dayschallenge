ad15dcf6df065e3a4e1d6e993b95b6efd64a5b89
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-06-25 10:36:52 +0530

    Add files via upload

---
+++ Day 3 - Email.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Email {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int t=obj.nextInt();
+        int n=obj.nextInt();
+        System.out.println(t-n);
+    }
+}
