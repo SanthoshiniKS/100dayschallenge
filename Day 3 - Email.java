84cef10e84b15d52162a8a5822ea5098aacf2611
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-06-25 10:28:27 +0530

    Delete Day 3 - Email.java

--- Day 3 - Email.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Email {
-    public static void main(String[] args) {
-        Scanner obj=new Scanner(System.in);
-        int t=obj.();
-        int n=obj.nextInt();
-        System.out.println(t-n);
-    }
-}￼
