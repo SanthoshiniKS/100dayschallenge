58a3b8407f927107835c0c9f8ff1b8433c418149
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-06-30 08:08:48 +0530

    Create Day 8 - InstNoodles.java

---
+++ Day 8 - InstNoodles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Noodles {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int x=obj.nextInt();
+        int y=obj.nextInt();
+        System.out.println(x*y);
+    }
+}￼
