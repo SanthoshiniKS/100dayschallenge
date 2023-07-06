5bc6b330b5c84788fa50dd42eaa3993b4f4be65a
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-07-06 08:46:07 +0530

    Create Day 14 - Qualify.java

---
+++ Day 14 - Qualify.java
@@ -0,0 +1,18 @@
+import java.utimport java.util.*;
+class Qualify{
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        while(n>0)
+        {
+            int x=obj.nextInt();
+            int a=obj.nextInt();
+            int b=obj.nextInt();
+            if((a*1+b*2)>=x)
+            System.out.println("Qualify");
+            else
+            System.out.println("Not Qualify");
+            n--;
+        }
+    }
+}
