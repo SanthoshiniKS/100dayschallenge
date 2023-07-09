57dcf490a11c3936e019b4870e327f1ddd524743
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-07-09 08:33:36 +0530

    Create Day 17 - NumMirror.java

---
+++ Day 17 - NumMirror.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class NumMirror {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        System.out.println(n);
+    }
+}
