271a217cc7a53f18de36f50b9831ef9f1fcc12e5
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-07-10 07:52:37 +0530

    Create  Day 18 - Movie.java

---
+++ Day 18-/Day 18 - Movie.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Movie {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int X=obj.nextInt();
+        int Y=obj.nextInt();
+        System.out.println(X-(Y/2));
+    }
+}
