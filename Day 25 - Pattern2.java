90da4fad18f027aeaaba51a3729095bd48195109
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-07-17 08:29:16 +0530

    Create Day 25 - Pattern2.java

---
+++ Day 25 - Pattern2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Pattern2 {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<i+1;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
