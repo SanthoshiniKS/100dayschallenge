1eea80205e8bf3cc35e961bdd290572869b5bb74
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-08-24 21:43:35 +0530

    Update Day 63 - Buyandsell.java

--- Day 63 - Buyandsell.java
+++ Day 63 - Buyandsell.java
@@ -1,7 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int max=0,min=Integer.MAX_VALUE;
-        System.out.println(min);
         for(int i=0;i<prices.length;i++)
         {
             if(prices[i]<min)
