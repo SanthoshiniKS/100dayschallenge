314ea82f9f5579fdc7b4db192371cc66ed6dfc79
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-08-27 14:28:22 +0530

    Create Day 66 - Leadersinanarray.java

---
+++ Day 66 - Leadersinanarray.java
@@ -0,0 +1,16 @@
+class Solution{
+    static ArrayList<Integer> leaders(int arr[], int n){
+         ArrayList<Integer> a=new ArrayList<>();
+         int max=arr[n-1];
+         a.add(max);
+         for(int i=n-2;i>=0;i--)
+         {
+             if(arr[i]>=max)
+             {
+                 max=arr[i];
+                 a.add(0,arr[i]);
+             }
+         }
+         return a;
+    }
+}￼
