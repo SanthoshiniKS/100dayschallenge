1003cf2e9ef1e1ca67aa8bd721b736045a3d66d5
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-09-02 23:03:59 +0530

    Update and rename Day 72 - Pascal Triangle to Day 72 - Pascal Triangle.java

--- Day 72 - Pascal Triangle.java
+++ Day 72 - Pascal Triangle.java
@@ -1,12 +1,12 @@
 lass Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList();
+        List<List<Integer>> res = new ArrayList<>();
         if (numRows<1) return res;
-        List<Integer> arr= new ArrayList();
+        List<Integer> arr= new ArrayList<>();
         arr.add(1);
         res.add(arr);
         for (int i=1;i<numRows;i++) {
-            List<Integer> cur=new ArrayList();
+            List<Integer> cur=new ArrayList<>();
             List<Integer> prev=res.get(i-1);
             for (int j=0;j<=i;j++) {
                 int temp=(j>0?prev.get(j-1):0)+(j<i?prev.get(j):0);
