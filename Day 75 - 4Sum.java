3ce9a6e156afbbf7ae2f3be2bbc21a090bb7f2be
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-09-05 22:12:12 +0530

    Update Day 75 - 4Sum.java

--- Day 75 - 4Sum.java
+++ Day 75 - 4Sum.java
@@ -3,11 +3,7 @@ class Solution {
         List<List<Integer>> arr=new ArrayList<>();
         Arrays.sort(nums);
         for (int i=0;i<nums.length;i++) {
-            if (i>0 && nums[i]==nums[i-1]) 
-                   continue;
             for (int j=i+1;j<nums.length;j++) {
-                if (j>i+1 && nums[j]==nums[j-1]) 
-                       continue;
                 int k=j+1;
                 int l=nums.length-1;
                 while (k<l) {
