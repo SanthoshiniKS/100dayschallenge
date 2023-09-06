9e39b3941e1c33d98d2cdd4882d67fbaafd677c3
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-09-06 22:49:02 +0530

    Update Day 76-LargestSubarray.java

--- Day 76-LargestSubarray.java
+++ Day 76-LargestSubarray.java
@@ -3,15 +3,15 @@ class GfG
     int maxLen(int arr[], int n)
     {
         HashMap<Integer,Integer> map=new HashMap<>();
-        int c=0,res=0,sum=0;
+        int c=0,res=0;
         for(int i=0;i<n;i++){
             c=c+arr[i];
-            if(c==sum)
+            if(c==0)
                 res=i+1;
             if(!map.containsKey(c))
                 map.put(c,i);
-            if(map.containsKey(c-sum))
-                res=Math.max(res,i-map.get(c-sum));
+            else
+                res=Math.max(res,i-map.get(c));
         }
         return res;
     }
