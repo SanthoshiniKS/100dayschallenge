cf554faf03984d7704a43429b3b3727ce841af50
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-09-10 20:05:21 +0530

    Update Day 80 -  MissingandRepeating.java

--- Day 80 -  MissingandRepeating.java
+++ Day 80 -  MissingandRepeating.java
@@ -1,8 +1,6 @@
 class Solve {
     int[] findTwoElement(int arr[], int n) {
         int[] temp=new int[n]; 
-        int repeatingNumber = -1;
-        int missingNumber = -1;
         int a[]=new int[2];
         for (int i=0;i<n;i++) {
             temp[arr[i]-1]++;
