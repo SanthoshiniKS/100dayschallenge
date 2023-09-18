5ded773a12e1f3be9a963e7e522c6716a887f9f1
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-09-18 10:23:51 +0530

    Create Day 88- Countoccurences.java

---
+++ Day 88- Countoccurences.java
@@ -0,0 +1,13 @@
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int c=0,l=0,h=n-1;
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            if(arr[mid]==x)
+             
+        }
+        return c;
+    }
+}
