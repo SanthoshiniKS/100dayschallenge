a75e0c6e8250e595c0913ed5b796e8fffc2057bf
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-09-18 19:45:44 +0530

    Update Day 88 - CountOccurences-Method2.java

--- Day 88 - CountOccurences-Method2.java
+++ Day 88 - CountOccurences-Method2.java
@@ -1,13 +1,10 @@
 class Solution {
     int count(int[] arr, int n, int x) {
-        // code here
         int c=0,l=0,h=n-1;
         int first=-1,last=-1;
-        while(l<=h)
-        {
+        while(l<=h){
             int mid=l+(h-l)/2;
-            if(arr[mid]==x)
-            {
+            if(arr[mid]==x){
                 first=mid;
                 h=mid-1;
             }
@@ -17,11 +14,9 @@ class Solution {
               l=mid+1;
         }
         l=0;h=n-1;
-        while(l<=h)
-        {
+        while(l<=h){
             int mid=l+(h-l)/2;
-            if(arr[mid]==x)
-            {
+            if(arr[mid]==x){
                 last=mid;
                 l=mid+1;
             }
